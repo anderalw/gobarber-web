4eac15e60742d5bdf65c333003747992b2f1419e
Anderson L. Wiedemann <anderalw@gmail.com>
2020-06-13 19:20:00 -0300

    Listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiPower, FiClock } from 'react-icons/fi';
 import {
   Container,
@@ -8,12 +8,15 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calender,
 } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
   return (
     <Container>
@@ -56,6 +59,56 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/62670548?s=460&u=3894c156289f9f1789755d82e20b38e2388ae501&v=4"
+                  alt="Anderson Wiedemann"
+                />
+                <strong>Anderson Wiedemann</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/62670548?s=460&u=3894c156289f9f1789755d82e20b38e2388ae501&v=4"
+                  alt="Anderson Wiedemann"
+                />
+                <strong>Anderson Wiedemann</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/62670548?s=460&u=3894c156289f9f1789755d82e20b38e2388ae501&v=4"
+                  alt="Anderson Wiedemann"
+                />
+                <strong>Anderson Wiedemann</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calender />
       </Content>
